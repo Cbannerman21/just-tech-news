72cef3a893fcc2bdfa1cfba08867702b8052bf81
Cbannerman <cbannerman21@gmail.com>
2022-06-10 11:15:47 -0400

    saving all work

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -38,7 +38,7 @@ router.get('/:id', (req, res) => {
 
 // POST /api/users
 router.post('/', (req, res) => {
-    // expects {username: 'Learnantino', email: 'learnantino@gmail.com', password: 'password1234'}
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
     User.create({
         username: req.body.username,
         email: req.body.email,
@@ -49,11 +49,29 @@ router.post('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
+ });
+
+ router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }) .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!'});
+            return;
+        }
+
+        res.json({ user: dbUserData });
+
+        // Verify user
+    })
 });
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
-    // expects {username: 'Learnantino', email: 'learnantino@gmail.com', password: 'password1234'}
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
     // pass in req.body instead to only update what's passed through
     User.update(req.body, {
